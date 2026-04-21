7b172f17935ce343b089222d2d9abdb1a7f3636a
vararais <varadigmaisnann@gmail.com>
2026-04-21 14:52:53 +0700

    Menambah kode User Service

--- deployy/src/main/java/com/praktikumDB/deployy/service/UserService.java
+++ deployy/src/main/java/com/praktikumDB/deployy/service/UserService.java
@@ -8,4 +8,23 @@ import java.util.UUID;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(User request){
+        request.setId(UUID.randomUUID().toString());
+        return userRepository.save(request);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public User getUserById(String id){
+        return userRepository.findById(id).orElse(null);
+    }
 }
